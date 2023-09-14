f5307d6ba177289890d581c5b7dee90add979fb4
Gilberto <codexfast@gmail.com>
2023-09-14 16:38:26 -0300

    some changes

---
+++ src/components/navbar.jsx
@@ -0,0 +1,22 @@
+
+
+const Navbar = () => {
+    return (
+        <nav className="container px-48">
+            <div className="bg-white h-48 flex items-center bet">
+                <h2 className="flex-none text-xl">Gilberto</h2>
+                <ul className="grow flex gap-12 justify-center">
+                    <li>About</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+                <div className="flex-none">
+                    O
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
